c8217ebb474c10c8743bf67a720c559428ff8be0
Noah <noah.murdow@gmail.com>
2016-11-14 03:34:45 -0200

    Responsive, SEO, Files Organization
    Responsive menu fix;
    Headings added for screen readers;
    Files Organization

---
+++ docs/meuhorario/behavior.js
@@ -0,0 +1,42 @@
+$(document).ready(function($) { 
+    $(".scroll").click(function(event){        
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+        $(".scroll").removeClass("activeLink");
+        $(this).addClass("activeLink");
+   });
+
+
+    $(window).scroll(function() {
+    	var scrollPosition = $(window).scrollTop();
+    	if($(window).width() > 960) {			   		
+	   		if(scrollPosition > 150) {
+	    		$('#topScreenMenu').slideDown();
+	   		}
+	   		else {
+	   			$('#topScreenMenu').slideUp();
+	   		}
+   		}
+   		if(scrollPosition > 120) {
+				$("#topScreenMenuLogo").removeClass("hidden");
+			}
+			else {
+				$("#topScreenMenuLogo").addClass("hidden");
+			}
+	});	
+
+	var isVisible = false;
+	$("#mobileMenuIcon").click(function(){
+		if(!isVisible) {
+			$("#mobileMenu").slideDown();
+			$("#mobileMenuIcon").attr("src","imagens/icones/menu-close-icon.png");
+			isVisible = true;
+		}
+		else {
+			$("#mobileMenu").slideUp();
+			$("#mobileMenuIcon").attr("src","imagens/icones/menu-icon.png");
+			isVisible = false;
+		}
+		
+	});	    
+});
