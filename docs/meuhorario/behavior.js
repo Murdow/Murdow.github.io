0d05692804df95db846b276c7b12539db9866f31
Noah <noah.murdow@gmail.com>
2016-11-29 04:37:54 -0200

    Content and layout canges
    Slideshow content change;
    Icons added to reviews;
    Arrows added to slideshow;
    Content betweeen slideshow and reviews change;
    New download button added to the end of the page;

--- docs/meuhorario/behavior.js
+++ docs/meuhorario/behavior.js
@@ -2,29 +2,44 @@ $(document).ready(function($) {
     $(".scroll").click(function(event){        
         event.preventDefault();
         $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
-        $(".scroll").removeClass("activeLink");
-        $(this).addClass("activeLink");
    });
 
 
     $(window).scroll(function() {
     	var scrollPosition = $(window).scrollTop();
     	if($(window).width() > 960) {			   		
-	   		if(scrollPosition > 150) {
-	    		$('#topScreenMenu').slideDown();
-	   		}
-	   		else {
-	   			$('#topScreenMenu').slideUp();
-	   		}
-   		}
-   		if(scrollPosition > 120) {
-				$("#topScreenMenuLogo").removeClass("hidden");
-			}
-			else {
-				$("#topScreenMenuLogo").addClass("hidden");
-			}
-	});	
+    	    if(scrollPosition > 150) {
+    	    	$('#topScreenMenu').slideDown();
+    	    }
+    	    else {
+    	   	   $('#topScreenMenu').slideUp();
+    	    }
+       	}
+       	if(scrollPosition > 120) {
+            $("#topScreenMenuLogo").removeClass("hidden");
+    	}
+    	else {
+            $("#topScreenMenuLogo").addClass("hidden");
+    	}
+        
+        //Altera a cor da seção ativa no menu
+        if(scrollPosition < 1700) {
+            $(".scroll").removeClass("activeLink");
+            $("#topScreenMenu ul li:nth-child(1) a").addClass("activeLink");
+        }
+        else {
+            if(scrollPosition >= 1700 && scrollPosition < 3000) {
+                $(".scroll").removeClass("activeLink");
+                $("#topScreenMenu ul li:nth-child(2) a").addClass("activeLink");
+            }
+            else {
+                $(".scroll").removeClass("activeLink");
+                $("#topScreenMenu ul li:nth-child(3) a").addClass("activeLink");
+            }
+        }
+    });	
 
+    //Alterna o icone do menu mobile(aberto/fechado)
 	var isVisible = false;
 	$("#mobileMenuIcon").click(function(){
 		if(!isVisible) {
